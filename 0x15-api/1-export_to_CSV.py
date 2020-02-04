83cdb14104b399b4ce5482557c833a5b9f337e2a
rcolbert30 <colbertrashaad@gmail.com>
2020-02-04 01:35:54 +0000

    csv

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+''' extension that exports data
+    into CSV format
+'''
+import csv
+from sys import argv
+from requests import get
+
+
+def todos(_id):
+    ''' function will take in argv1 to return sed ID'''
+    user = get("https://jsonplaceholder.typicode.com/users",
+               params={'id': _id}).json()
+    todo = get("https://jsonplaceholder.typicode.com/todos",
+               params={'userId': _id}).json()
+    progress = get("https://jsonplaceholder.typicode.com/todos",
+                   params={'userId': _id, 'completed': 'true'}).json()
+
+    name = user[0].get('name')
+    tasks = len(todo)
+    prog = len(progress)
+
+    print("Employee {} is done with tasks({}/{}):".format(name, prog, tasks))
+
+    for task in progress:
+        print("\t {}".format(task["title"]))
+
+    with open('{}.csv'.format(_id), 'w', encoding='utf-8') as user_file:
+        data = csv.writer(user_file, quoting=csv.QUOTE_ALL)
+
+        for task in todo:
+                data.writerow([_id, name, task.get('completed'),
+                               task.get('title')])
+
+
+if __name__ == "__main__":
+    if argv[1].isdigit():
+        todos(argv[1])
