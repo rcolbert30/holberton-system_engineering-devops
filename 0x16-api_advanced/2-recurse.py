58e4055e49d9ee0d539e7fee8cc5878e498d6b98
rcolbert30 <colbertrashaad@gmail.com>
2020-02-07 06:25:28 +0000

    recurse

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,13 +3,14 @@
 
 import requests
 
+
 def recurse(subreddit, after='', hot_list=[]):
     url = 'https://api.reddit.com/r/{}/hot'.format(subreddit)
     headers = {'User-Agent': 'Hot Post Recursive Grab Info'}
     x = {'after': after}
     try:
         data = requests.get(url, headers=headers, params=paginate,
-                                   allow_redirects=False).json()
+                            allow_redirects=False).json()
         articles = reddit_data['data']['children']
 
         for article in articles:
